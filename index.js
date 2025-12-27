a1ecac80c03f29242b892d96280052b8cb7f71f3
Harshit Bisht <gitwebdevlopmentproject@gmail.com>
2025-12-27 07:07:57 +0530

    Create login signup homepage skeleton and basic get routes setup

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/homepage.html");
 });
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
